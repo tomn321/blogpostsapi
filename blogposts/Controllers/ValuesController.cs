dc22481ca2dfee159f806c6ff966434561bb530b
tnguyen <aftom321@gmail.com>
2018-03-21 13:32:40 -0400

    blog posts

---
+++ blogposts/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using blogposts.Controllers.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blogposts.Controllers
+{
+    [Route("api/[controller]")]
+    public class PostsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Post> Get()
+        {
+            return new Post[] { new Post{
+                UserId = 1,
+                Id = 1,
+                Title = "First test post",
+                Body = "This post\nis a test\non my new site."
+             }};
+        }
+
+      
+    }
+}
---
+++ blogposts/Controllers/models/post.cs
@@ -0,0 +1,10 @@
+namespace blogposts.Controllers.models
+{    
+    public class Post
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
